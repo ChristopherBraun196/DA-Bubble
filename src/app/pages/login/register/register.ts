cc33b5d518490461a31fef50ec92c282107b03f9
Moritz Böhm <ml-boehm@gmx.de>
2026-09-18 02:56:13 +0200

    feat: implement registration page UI

--- src/app/pages/login/register/register.ts
+++ src/app/pages/login/register/register.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 import { Header } from '../shared/header/header';
 
 @Component({
-  imports: [Header],
+  imports: [Header, ReactiveFormsModule, RouterLink],
   selector: 'app-register',
   styleUrl: './register.scss',
   templateUrl: './register.html',
 })
-export class Register {}
+export class Register {
+  protected readonly form = new FormGroup({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    privacyAccepted: new FormControl(false, {
+      nonNullable: true,
+      validators: [Validators.requiredTrue],
+    }),
+  });
+
+  protected onSubmit(): void {
+    this.form.markAllAsTouched();
+  }
+}
