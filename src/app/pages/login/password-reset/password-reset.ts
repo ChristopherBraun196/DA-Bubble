8f5d3350fbf0ea808a16f891a356d4c68567aaf6
ChristopherBraun196 <232182424+ChristopherBraun196@users.noreply.github.com>
2026-09-18 18:35:38 +0200

    feat: implement password reset functionality with email input and validation

--- src/app/pages/login/password-reset/password-reset.ts
+++ src/app/pages/login/password-reset/password-reset.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Header } from '../shared/header/header';
+import { RouterLink } from '@angular/router';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [Header, RouterLink, ReactiveFormsModule],
   selector: 'app-password-reset',
   styleUrl: './password-reset.scss',
   templateUrl: './password-reset.html',
 })
-export class PasswordReset {}
+export class PasswordReset {
+  form = new FormGroup({
+    email: new FormControl(''),
+  });
+
+  clearLoginError() {}
+}
