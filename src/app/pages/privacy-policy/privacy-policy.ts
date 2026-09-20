333f3278dbb775d707e7e4e926c770b61ef06a2f
ChristopherBraun196 <232182424+ChristopherBraun196@users.noreply.github.com>
2026-09-20 11:16:59 +0200

    feat: implement back navigation in privacy policy page

--- src/app/pages/privacy-policy/privacy-policy.ts
+++ src/app/pages/privacy-policy/privacy-policy.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { Header } from '../login/shared/header/header';
 
 @Component({
-  imports: [RouterLink, Header],
+  imports: [Header],
   selector: 'app-privacy-policy',
   styleUrl: './privacy-policy.scss',
   templateUrl: './privacy-policy.html',
 })
-export class PrivacyPolicy {}
+
+// Back Button return to last page function
+export class PrivacyPolicy {
+  private readonly location = inject(Location);
+  private readonly router = inject(Router);
+
+  protected goBack(): void {
+    if (history.state?.navigationId > 1) {
+      this.location.back();
+      return;
+    }
+
+    this.router.navigate(['/login']);
+  }
+}
